daf79f4fd585a01df41aa7538fcaf34604ebe003
Fengqi Wang <fengqi.wang@gmail.com>
2015-04-30 13:04:58 -0700

    Update MainActivity.java

--- app/src/main/java/com/fengqi/locationote/MainActivity.java
+++ app/src/main/java/com/fengqi/locationote/MainActivity.java
@@ -38,7 +38,21 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         mApiClient.connect();
 
         mGeofenceList = new ArrayList<Geofence>();
+        
+        mGeofenceList.add(new Geofence.Builder()
+    // Set the request ID of the geofence. This is a string to identify this
+    // geofence.
+    .setRequestId(entry.getKey())
 
+    .setCircularRegion(
+            entry.getValue().latitude,
+            entry.getValue().longitude,
+            Constants.GEOFENCE_RADIUS_IN_METERS
+    )
+    .setExpirationDuration(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+            Geofence.GEOFENCE_TRANSITION_EXIT)
+    .build());
     }
 
 
