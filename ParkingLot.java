7ec58c35cc37fcff593b6ccb85c58dec6bb8f3cd
Fengqi Wang <fengqi.wang@gmail.com>
2015-05-25 17:17:43 -0700

    Update ParkingLot.java

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,92 +1,173 @@
-package com.fengqi.plms;
+package com.test;
 
-import java.util.HashMap;
-import java.util.Map;
+public class PKMS {
 
-public class ParkingLot {
-public static void main(char[] args) {
-	System.out.println("HHH");
-}
+  public class Car {
 
-public class Car {
-	public String plate;
-}
+    public String plate;
+  }
 
-private static final int MAX_SPACES = 100;
+  public class Entrence {
 
-private static final int NUM_OF_ENTRENCES = 5;
-private static final int NUM_OF_EXITS = 4;
+    private int number; 
 
-private Car[] parkings;
+    public void open() {
+      // TBD
+    }
 
-private Entrence[] entrences = new Entrence[NUM_OF_ENTRENCES];
-private Exit[] exits = new Exit[NUM_OF_EXITS];
+    public void close() {
+      // TBD, delay some seconds for safety;
+    }
+  }
 
-public class Entrence {
-	private int number;
-	
-	public void open() {
-		
-	}
-	public void close() {
-		
-	}
-}
+  public class Exit {
 
-public class Exit {
-	private int number;
-	
-	public void open() {
-		
-	}
-	public void close() {
-		
-	}
-}
+    private int number;
 
-void initialize() {
-	parkings = new Car[MAX_SPACES];
-		
-	for (Entrence entr : entrences) {
-		entr.close();
-	}
-	
-	for (Exit ex : exits) {
-		ex.close();
-	}
-}
+    public void open() {
+      // TBD
+    }
 
-private int findAvailableSpace() {
-	int result = -1;
-	for(int i = 0; i < parkings.length; i++) {
-		if (parkings[i] == null) {
-			result = i;
-			break;
-		}
-	}
-	return result;
-}
+    public void close() {
+      // TBD, assuming the hardware will delay some seconds for safety;
+    }
+  }
 
-private void issueTicket(Car car) {
-	
-}
+  public class Display {
 
-private void charge(Car car) {
-	
-}
+    private int freeSpaces;
 
-void handleArrival(Car car, Entrence entr) {
-	int found = findAvailableSpace();
-	if (found != -1) {
-		entr.open();
-		issueTicket(car);
-		entr.close();
-	}
-}
+    public synchronized void update(int spaces) {
+      freeSpaces = spaces;
+    }
 
-void handleLeaving(Car car, Exit ex) {
-	charge(car);
-	ex.open();
-	ex.close();
-}
+    public synchronized void addSpaces() {
+      freeSpaces++;
+    }
+
+    public synchronized void minusSpaces() {
+      freeSpaces--;
+    }
+  }
+  
+  public static enum Direction {    
+    ARRIVAL,
+    LEAVING
+  }
+  
+  public class Event { 
+    public Direction dir;
+    public Car getCar() {
+     return null; //TBD 
+    }
+    public Entrence getEntrence() {
+      return null; //TBD
+    }
+    public Exit getExit() {
+      return null; //TBD
+    }
+    public int number;  //could be for an entrence or an exit.
+  }
+  
+
+  private static final int MAX_SPACES = 100;
+
+  private static final int NUM_OF_ENTRENCES = 5;
+
+  private static final int NUM_OF_EXITS = 4;
+
+  private Car[] parkings = new Car[MAX_SPACES];;
+
+  private Entrence[] entrences = new Entrence[NUM_OF_ENTRENCES];
+
+  private Exit[] exits = new Exit[NUM_OF_EXITS];
+
+  private Display display = new Display();
+
+  public void initialize() {
+    for (int i = 0; i < parkings.length; i++) {
+      parkings[i] = null;
+    }
+
+    for (int i = 0; i < entrences.length; i++) {
+      entrences[i].number = i;
+      entrences[i].close();
+    }
+
+    for (int i = 0; i < exits.length; i++) {
+      exits[i].number = i;
+      exits[i].close();
+    }
+
+    display.update(MAX_SPACES);
+  }
+  
+  public void run() {
+    while(true) {
+      Event evt = nextEvent();
+      if(evt.dir.equals(Direction.ARRIVAL)) {
+        handleArrival(evt.getCar(), evt.getEntrence());
+      }
+      else if (evt.dir.equals(Direction.LEAVING)) {
+        handleLeaving(evt.getCar(), evt.getExit());
+      }
+    }
+  }
+
+  public void handleArrival(Car car, Entrence entr) {
+    int found = findAvailableSpace();
+    if (found != -1) {
+      entr.open();
+      issueTicket(car);
+      entr.close();
+      display.minusSpaces();
+    }
+  }
+
+  public void handleLeaving(Car car, Exit ex) {
+    if(verifyPayment(car)) {
+      updateAvailableSpace(car);
+      display.addSpaces();
+      ex.open();
+      ex.close();
+    }    
+  }
+  
+  private Event nextEvent() {
+    return null; //TBD, should involve the sensors system.
+  }
+
+  private synchronized int findAvailableSpace() {
+    int result = -1;
+    for (int i = 0; i < parkings.length; i++) {
+      if (parkings[i] == null) {
+        result = i;
+        break;
+      }
+    }
+    return result;
+  }
+
+  private synchronized void updateAvailableSpace(Car car) {
+    for (int i = 0; i < parkings.length; i++) {
+      if (parkings[i].equals(car)) {
+        parkings[i] = null;
+      }
+    }
+  }
+
+  private void issueTicket(Car car) {
+     //TBD, to track the parking time, etc.
+  }
+
+  private boolean verifyPayment(Car car) {
+    //TBD, handling the payment here.
+    return true;
+  }
+  
+  public static void main(char[] args) {
+    PKMS parkingLot = new PKMS();
+    parkingLot.initialize();    
+    parkingLot.run();
+  }
 }
